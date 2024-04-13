6b9f0c2ed977ed89f95b4a7e84abe45543bc2b93
Jinho Lee <50613940+binary-h0@users.noreply.github.com>
2024-04-13 18:37:39 +0900

    Title: 코드트리 메신저, Time: 8010ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

---
+++ 240413/코드트리 메신저/codetree-messenger.py
@@ -0,0 +1,67 @@
+import sys
+sys.setrecursionlimit = 10**8
+input = lambda: sys.stdin.readline().strip()
+
+N, Q = map(int, input().split())
+query = list(map(int, input().split()))
+cmd, parents, authorities = query[0], query[1:N+1], query[N+1:N*2 + 1]
+alerts = [True for _ in range(N + 1)]
+answers = [0 for _ in range(N + 1)]
+
+def update_count_recursive(c, power, alert):
+    global answers
+    if not alert:
+        return
+    answers[c] += 1
+    if (power == 0) or (c == 0):
+        return
+    update_count_recursive(parents[c-1], power - 1, alerts[c])
+
+def calc_count_can_alert_chat_room():
+    global answers
+    answers = [0 for _ in range(N + 1)]
+    for c in range(1, N + 1):
+        power = authorities[c-1]
+        update_count_recursive(parents[c-1], power-1, alerts[c])
+        
+
+def set_alert(args):
+    global alerts
+    c = args[0]
+    alerts[c] = not alerts[c]
+
+def set_authority(args):
+    global authorities
+    c, power = args[0], args[1]
+    authorities[c-1] = power
+
+def set_parent(args):
+    global parents
+    c1, c2 = args[0] - 1, args[1] - 1
+    tmp = parents[c1]
+    parents[c1] = parents[c2]
+    parents[c2] = tmp
+
+def get_count_can_alert_chat_room(args):
+    c = args[0]
+    return answers[c]
+
+def controller(cmd, args, is_updated):
+    if cmd == 200:
+        set_alert(args)
+    elif cmd == 300:
+        set_authority(args)
+    elif cmd == 400:
+        set_parent(args)
+    elif cmd == 500:
+        if not is_updated:
+            calc_count_can_alert_chat_room()
+        print(get_count_can_alert_chat_room(args))
+
+
+if __name__=="__main__":
+    is_updated = False
+    for _ in range(Q - 1):
+        query = list(map(int, input().split()))
+        cmd, args = query[0], query[1:]
+        controller(cmd, args, is_updated)
