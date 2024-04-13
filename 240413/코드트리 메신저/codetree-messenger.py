7211206676e7f9ac6cd781c0586555797b9fc941
Jinho Lee <50613940+binary-h0@users.noreply.github.com>
2024-04-13 22:11:14 +0900

    Title: 코드트리 메신저, Time: 8012ms, Memory: 63MB, Status: Time Limit Exceed - CodeTree

--- 240413/코드트리 메신저/codetree-messenger.py
+++ 240413/코드트리 메신저/codetree-messenger.py
@@ -1,3 +1,4 @@
+from collections import deque
 import sys
 sys.setrecursionlimit = 10**8
 input = lambda: sys.stdin.readline().strip()
@@ -5,8 +6,13 @@ input = lambda: sys.stdin.readline().strip()
 N, Q = map(int, input().split())
 query = list(map(int, input().split()))
 cmd, parents, authorities = query[0], query[1:N+1], query[N+1:N*2 + 1]
+graph = [set() for _ in range(N+1)]
 alerts = [True for _ in range(N + 1)]
 answers = [0 for _ in range(N + 1)]
+
+for i in range(N):
+    graph[parents[i]].add(i+1)
+# print(graph)
 
 def update_count_recursive(c, power, alert):
     global answers
@@ -23,7 +29,22 @@ def calc_count_can_alert_chat_room():
     for c in range(1, N + 1):
         power = authorities[c-1]
         update_count_recursive(parents[c-1], power-1, alerts[c])
-        
+
+def get_alert_room_count(args):
+    que = deque()
+    que.append((args[0], 1))
+    answer = 0
+    while que:
+        room, power = que.popleft()
+        for nr in graph[room]:
+            if not alerts[nr]:
+                continue
+            npower = authorities[nr-1]
+            if power <= npower:
+                answer += 1
+            que.append((nr, power + 1))
+    print(answer)            
+    
 
 def set_alert(args):
     global alerts
@@ -38,9 +59,14 @@ def set_authority(args):
 def set_parent(args):
     global parents
     c1, c2 = args[0] - 1, args[1] - 1
-    tmp = parents[c1]
-    parents[c1] = parents[c2]
-    parents[c2] = tmp
+    p1, p2 = parents[c1], parents[c2]
+    parents[c1] = p2
+    parents[c2] = p1
+    graph[p1].remove(c1+1)
+    graph[p2].remove(c2+1)
+    graph[p1].add(c2+1)
+    graph[p2].add(c1+1)
+    # print(graph)
 
 def get_count_can_alert_chat_room(args):
     c = args[0]
@@ -57,10 +83,11 @@ def controller(cmd, args, is_updated):
         set_parent(args)
         is_updated = False
     elif cmd == 500:
-        if not is_updated:
-            calc_count_can_alert_chat_room()
-            is_updated = True
-        print(get_count_can_alert_chat_room(args))
+        get_alert_room_count(args)
+        #if not is_updated:
+        #    calc_count_can_alert_chat_room()
+        #    is_updated = True
+        #print(get_count_can_alert_chat_room(args))
 
 
 if __name__=="__main__":
